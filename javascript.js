e6e09959d67e1e109c37b2a32a4d105a82ef6d89
kima2017 <kimabaghdasaryan@gmail.com>
2020-06-28 16:27:18 +0400

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,43 +1,123 @@
-/*1. կոճակի քլիկի ժամանակ կհաշվվի և մեր պատուհանի մեջ կտպվի բոլոր երկնիշ զույգ թվերի գումարը։
+const l = console.log;
+// let names = ["Martin", "Kima", "Elen", "Gagik"];
+
+// // l(names[0]);
+// // l(names[1]);
+// // l(names.length);
+// // l(names[names.length - 1]);
+// l(names);
+// names.unshift("Hrach");
+// l(names);
+// names.shift();
+// l(names);
+// names.push("Hrach");
+// l(names);
+// names.pop();
+// l(names);
+
+// names.splice(2, 0, "Nazik", "Mher");
+// l(names);
+
+// for(let i = 0; i<names.length; i++) {
+//     document.write(names[i] + "<br>");
+// }
+
+// function insertNames() {
+//     let names = [];
+//     let value = document.getElementById("name").value;
+//     for (let i = 0; i<value; i++) {
+//         let numbers = Math.floor(Math.random()*100);
+//         names.push(numbers);
+//     }
+//     l(names);
+// }
+
+
+// let gumar = 0; 
+// for(let i = 0; i <arr.length; i++) {
+//     gumar = gumar + arr[i];
+// }
+// l(gumar);
+
+// let arr = [-81, -6, -68, -51, -14];
+// let min = arr[0];
+// for(let i = 0; i < arr.length; i++) {
+//     if(min > arr[i]){
+//         min = arr[i];
+//     }
+// }
+// l(min);
+
+/*
+function insertNames() {
+
+    let number1 = Number(document.getElementById("name").value);
+    let number2 = Number(document.getElementById("name2").value);
+    let number3 = Number(document.getElementById("name3").value);
+    let datark = [];
+    
+    for(let i = 0; i<number1; i++) {
+        let ispush = true;
+        let result = Math.floor(Math.random()*(number3 - number2)+number2);
+        l(result);
+        for(let j = 0; j<datark.length; j++) {
+            if(datark[j] == result) {
+                ispush = false;
+                break;
+            }
+        }
+        if(ispush){
+            datark.push(result)
+        } else { 
+            i--;
+        }
+    }
+   l(datark);
+}
 */
+// let x = 5;
+// let y = 20;
 
-let gumar = 0;
-function countEvenNumSum() {
-for (let i = 10; i <=99; i++) {
-    if(i%2===0) {
-      gumar = gumar + i;
+// let result = Math.floor(Math.random()*(y-x)+x);
+// l(result);
+
+// 0 + 5 = 5; 
+// 15 + 5 = 20;
+function getOddSum() {
+let gumar1 = 0;
+let numbers1 = [1, 3, 17, 12, 8, 7, 9, 9, 1 , 3, 12 , 15, 5];
+for(let i = 0; i < numbers1.length; i++) {
+    if(numbers1[i]%2!==0) {
+        gumar1 = gumar1 + numbers1[i];
     }
 }
-document.write(gumar);
+console.log(gumar1);
 }
 
-
-/*	2. կոճակի քլիկի ժամանակ կհաշվվի և մեր պատուհանի մեջ կտպվի բոլոր միանիշ թվերի արտադրյալը։
-*/
-
-let product = 1;
-function countOneDigitNumProduct() {
-for (let i = 1; i <=9; i++) {
-      product = product * i;
-}
-document.write(product);
-}
-
-/*3. կոճակի քլիկի ժամանակ մեր պատուհանի մեջ կտպվեն բոլոր երկնիշ թվերը, և՝ եթե տվյալ թիվը առանց մնացորդի 
-բաժանվում է 3 -ի ապա նրա դիմաց կգրվի "A", եթե տվյալ թիվը առանց մնացորդի բաժանվում է 5 -ի ապա նրա 
-դիմաց կգրվի B, իսկ եթե  թիվը բաժանվում է և 5 -ի և 3 -ի ապա նրա դիմաց կգրվի AB: */
-
-function countTwoDigitNumProduct() {
-for (let i = 10; i <=99; i++) {
-    document.write(i + "<br>");
-    if(i%3===0) {
-        document.write(i + " " + "A" + "<br>");
-    }  
-    if(i%5===0) {
-        document.write(i + " " + "B" + "<br>");
+function getEvenProduct() {
+    let gumarOdd = 1;
+    let numbers2 = [1, 3, 17, 12, 8, 7, 9, 9, 1 , 3, 12 , 15, 5];
+    for(let i = 0; i < numbers2.length; i++) {
+        if(numbers2[i]%2===0) {
+            gumarOdd = gumarOdd * numbers2[i];
+        }
     }
-    if(i%5===0 && i%3===0) {
-        document.write(i + " " + "AB" + "<br>");
-    }
+    console.log(gumarOdd);
 }
+
+//Հաշվել Զանգվածի 3-ին բաժանվող թվերի միջին թվաբանական (Գումար / քանակ)
+
+function getAvarage() {
+    let gumarThree = 0;
+    let avarage;
+    let newArray = [];
+    let numbers3 = [1, 3, 17, 12, 8, 7, 9, 9, 1 , 3, 12 , 15, 5];
+    for(let i = 0; i < numbers3.length; i++) {
+        if(numbers3[i]%3===0) {
+            newArray.push(numbers3[i]);
+            gumarThree = gumarThree + numbers3[i];
+            avarage = gumarThree / newArray.length;
+        }
+    }
+    console.log(avarage);
 }
